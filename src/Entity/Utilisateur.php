0d25620c1893f6e59aa1ad0533b22c369eaab60d
Julien LEFEVRE <julien.lefevre2025@campus-eni.fr>
2025-09-03 10:00:55 +0200

    remember me finit

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $phoneNumber = null;
+
+    #[ORM\Column]
+    private ?bool $actif = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'utilisateurs')]
+    private ?self $campus = null;
+
+    /**
+     * @var Collection<int, self>
+     */
+    #[ORM\OneToMany(targetEntity: self::class, mappedBy: 'campus')]
+    private Collection $utilisateurs;
+
+    /**
+     * @var Collection<int, Outing>
+     */
+    #[ORM\OneToMany(targetEntity: Outing::class, mappedBy: 'utilisateur')]
+    private Collection $outingOrganizer;
+
+    /**
+     * @var Collection<int, outing>
+     */
+    #[ORM\ManyToMany(targetEntity: outing::class, inversedBy: 'utilisateurs')]
+    private Collection $outingParticipants;
+
+    public function __construct()
+    {
+        $this->utilisateurs = new ArrayCollection();
+        $this->outingOrganizer = new ArrayCollection();
+        $this->outingParticipants = new ArrayCollection();
+    }
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+        // @deprecated, to be removed when upgrading to Symfony 8
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function isActif(): ?bool
+    {
+        return $this->actif;
+    }
+
+    public function setActif(bool $actif): static
+    {
+        $this->actif = $actif;
+
+        return $this;
+    }
+
+    public function getCampus(): ?self
+    {
+        return $this->campus;
+    }
+
+    public function setCampus(?self $campus): static
+    {
+        $this->campus = $campus;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getUtilisateurs(): Collection
+    {
+        return $this->utilisateurs;
+    }
+
+    public function addUtilisateur(self $utilisateur): static
+    {
+        if (!$this->utilisateurs->contains($utilisateur)) {
+            $this->utilisateurs->add($utilisateur);
+            $utilisateur->setCampus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUtilisateur(self $utilisateur): static
+    {
+        if ($this->utilisateurs->removeElement($utilisateur)) {
+            // set the owning side to null (unless already changed)
+            if ($utilisateur->getCampus() === $this) {
+                $utilisateur->setCampus(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Outing>
+     */
+    public function getOutingOrganizer(): Collection
+    {
+        return $this->outingOrganizer;
+    }
+
+    public function addOutingOrganizer(Outing $outingOrganizer): static
+    {
+        if (!$this->outingOrganizer->contains($outingOrganizer)) {
+            $this->outingOrganizer->add($outingOrganizer);
+            $outingOrganizer->setUtilisateur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOutingOrganizer(Outing $outingOrganizer): static
+    {
+        if ($this->outingOrganizer->removeElement($outingOrganizer)) {
+            // set the owning side to null (unless already changed)
+            if ($outingOrganizer->getUtilisateur() === $this) {
+                $outingOrganizer->setUtilisateur(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, outing>
+     */
+    public function getOutingParticipants(): Collection
+    {
+        return $this->outingParticipants;
+    }
+
+    public function addOutingParticipant(outing $outingParticipant): static
+    {
+        if (!$this->outingParticipants->contains($outingParticipant)) {
+            $this->outingParticipants->add($outingParticipant);
+        }
+
+        return $this;
+    }
+
+    public function removeOutingParticipant(outing $outingParticipant): static
+    {
+        $this->outingParticipants->removeElement($outingParticipant);
+
+        return $this;
+    }
+
+
+}
